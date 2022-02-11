7c3cc486f9b110dc4f6e016f7bb7ea2c34c59360
Lucas <43614096+Twistere@users.noreply.github.com>
2022-02-11 09:00:53 +0100

    fix: final schema for the bdd

--- src/services/image-service.js
+++ src/services/image-service.js
@@ -1,39 +1,41 @@
-const  { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client')
 const prisma = new PrismaClient()
 const xmp = require('exifr')
 
 
 
-const insertImg = async (filePath) =>{
-    let data = await xmp.parse(filePath, {xmp: true})
-    console.log(data.FlightYawDegree)
-
+const insertImg = async (date) => {
     await prisma.user.create({
-        data : {
-            date : new Date(Date.now()).toISOString(),
-            cadastre : {
-                create : {
-                    url : 'google.com',
-                    image : {
-                        create : {
-                            pathImg: filePath,
-                            gpsLatitude : data.GpsLatitude,
-                            gpsLongitude : data.GpsLongitude,
-                            gimballYawDegree : data.FlightYawDegree,
-                            absoluteAltitude : data.AbsoluteAltitude
-
-                        }
-                    }    
-                },
-                
-            },
-            
+        data: {
+            date: (new Date(date).toISOString()),
         }
     })
 
 
 }
 
+const insertImg2 = async (filePath) => {
+    
+    let data = await xmp.parse(filePath, { xmp: true })
+    console.log(data.FlightYawDegree)
+    await prisma.image.create({
+        data: {
+            pathImg: filePath,
+            gpsLatitude: data.GpsLatitude,
+            gpsLongitude: data.GpsLongitude,
+            gimballYawDegree: data.FlightYawDegree,
+            absoluteAltitude: data.AbsoluteAltitude,
+
+            cadastre : {
+                connect : {
+                    idCadastre : 1
+                }
+            }
+        }
+    })
+}
+
 module.exports = {
-    insertImg
+    insertImg,
+    insertImg2
 }
