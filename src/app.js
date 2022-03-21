f312de960880c682a1d502c95a5293f427f63317
Twistere <43614096+Twistere@users.noreply.github.com>
2022-03-21 10:38:42 +0100

    feat: add api

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ const multer = require("multer");
 const path = require("path");
 
 const { fetchUser } = require("./services/user-service");
+const res = require("express/lib/response");
+
+app.set('views', '/home/Lucas/development/mapbox-frontend/dist');
+app.set('view engine', 'html')
+app.set('view engine', 'ejs');
 
 const imageUpload = multer({
   storage: multer.diskStorage({
@@ -31,7 +36,7 @@ app.use("/api", imageUpload.array("upload", 12), async (req, res) => {
 
   for (let i = 0; i < filePath.length; i++) await insertImg(filePath[i].path);
 
-  res.json("/upload api");
+  res.redirect(200, 'http://vps-02643af6.vps.ovh.net');
 });
 
 app.get("/image/:filename", (req, res) => {
@@ -45,6 +50,20 @@ app.get("/user", async (req, res) => {
   let user = fetchUser();
   user.then(function (result) {
     console.log(result);
-    return res.send(result);
+    res.render("index", {user: result});
   });
 });
+
+
+app.get("/", async (req, res) =>{
+  res.sendFile("/home/Lucas/development/mapbox-frontend/dist/index.html")
+})
+
+app.get("/style", async (req, res) => {
+  res.sendFile("/home/Lucas/development/mapbox-frontend/dist/style.css")
+})
+
+app.get("/bundle", async (req, res) => {
+  res.sendFile("/home/Lucas/development/mapbox-frontend/dist/bundle.js")
+})
+
