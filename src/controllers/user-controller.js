2cb9832d8fab0362a44a77e2cafa2c4f26026330
Lucas <43614096+Twistere@users.noreply.github.com>
2022-04-05 14:53:18 +0200

    feat: add controller for user

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,10 +1,22 @@
 const { fetchDate } = require('../services/user-service');
 
-exports.displayUser = (req, res) => {
+const  displayUser = (req, res) => {
     let user = fetchDate();
     user.then(function (result) {
-      console.log(result);
-      res.json(result);
+      res.render("index", {user: result});
     });
-    
+}
+
+
+const  displayUserJson = (req, res) => {
+  let user = fetchDate();
+  user.then(function (result) {
+    res.json(result);
+  });
+}
+
+
+module.exports = {
+  displayUser,
+  displayUserJson
 }
